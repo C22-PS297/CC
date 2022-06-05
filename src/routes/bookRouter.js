57a9f2a1dc53b45a632b8ac34bbc30cf2aa4f896
AlfanArzaqi <alfanmarzaqi@gmail.com>
2022-06-05 17:24:27 +0700

    edit bookRouter

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,14 +1,7 @@
 import { addBook, getBookbyuserID } from "../handler/bookControl.js";
-import {
-    getUserById,
-} from "../handler/userControl.js";
 import express from "express";
 
-const userRoute = express.Router();
 const bookRoute = express.Router();
-
-
-// userRoute.get("/:id", getUserById);
 
 bookRoute.post("/", addBook);
 bookRoute.get("/:id", getBookbyuserID);
