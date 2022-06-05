65ef5c26aa26870c173a55686533bc0ea7a081ea
AlfanArzaqi <alfanmarzaqi@gmail.com>
2022-06-05 20:04:40 +0700

    Update shop getAllShop and getShopbyId

--- src/handler/shopControl.js
+++ src/handler/shopControl.js
@@ -1,6 +1,14 @@
-import { addDoc, collection } from "firebase/firestore";
+import { async } from "@firebase/util";
+import { 
+    addDoc, 
+    collection, 
+    getDocs, 
+    getDoc,
+    doc,
+} from "firebase/firestore";
 import db from "../database/db.js";
 import shop from "../model/shop.js";
+import user from "../model/user.js";
 
 export async function addShop (request, response) {
     const {
@@ -41,4 +49,57 @@ export async function addShop (request, response) {
         })
     })
     return;
+};
+
+export async function getAllShop (request, response) {
+    try {
+        const shopCol = collection(db, 'shop');
+        const shopSnap = await getDocs (shopCol);
+        const shops = shopSnap.docs.map(shop => shop.data());
+
+        response.status(200).json({
+            status: "Success",
+            message: "Data successfully obtained",
+            data : shops
+        });
+        return;
+    } catch (err) {
+        response.status(500).json({
+            status: "Fail",
+            message: "Not available"
+        });
+        return;
+    }
+};
+
+export async function getShopById (request, response) {
+    const id = request.params.id;
+    const idRef = doc(db, 'shop', id);
+    await getDoc(idRef).then(userSnap => {
+        if (!userSnap.exists()) {
+            response.status(404).json({
+                status: "Fail",
+                message: "User not found"
+            });
+            return;
+        }
+
+        const shop = {
+            id: userSnap.id,
+            ...userSnap.data()
+        }
+
+        response.status(200).json({
+            status: "Success",
+            message: "User successfully obtained",
+            data: shop
+        })
+        return;
+    }).catch(err => {
+        response.status(500).json({
+            status: "Fail",
+            message: "Not available"
+        })
+        return;
+    });
 };
--- src/routes/shopRouter.js
+++ src/routes/shopRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { addShop } from "../handler/shopControl.js";
+import { addShop, getAllShop, getShopById } from "../handler/shopControl.js";
 
 const shopRouter = express.Router();
 
 shopRouter.post("/", addShop);
+shopRouter.get("/", getAllShop);
+shopRouter.get("/:id", getShopById);
 
 export default shopRouter;
