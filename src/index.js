31bec4d5cf9bb0282a089a33fabfe094181b9e40
AlfanArzaqi <alfanmarzaqi@gmail.com>
2022-06-05 17:46:56 +0700

    edit bookRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express, { json } from 'express'
+import cors from 'cors';
 import userRoute from './routes/userRouter.js'
 import bookRoute from './routes/bookRouter.js'
 import shopRouter from './routes/shopRouter.js';
@@ -6,7 +7,7 @@ import shopRouter from './routes/shopRouter.js';
 const port = 7777;
 
 const app=express()
-
+app.use(cors());
 app.use(json());
 
 app.use('/user', userRoute);
